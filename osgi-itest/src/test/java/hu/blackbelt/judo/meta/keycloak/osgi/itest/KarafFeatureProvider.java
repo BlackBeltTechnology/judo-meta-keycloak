2992681eb4539fae8b8aa08c766c943273159cc9
Robert Csakany <csakany.robert@gmail.com>
2022-09-22 03:15:37 +0200

    JNG-3834 Aggregate karaf features (#8)
    * JNG-3834 Aggregate karaf features
    
    * JNG-3834 Bump versions
    
    * JNG-3834 Bump versions and licence update
    
    * JNG-3834 Licences and versions updated
    
    * JNG-2834 Updating versions and fixing karaf provider
    
    * JNG-3834 Fixing CI scripts
    
    * JNG-3834 Bump dependencies
    
    * JNG-3834 Add license headers
    
    * Add javadoc generatrion
    
    * JNG-3834 Fixing versions in release
    
    * JNG-3834 Add bump version and release script fixes
    
    * JNG-3834 Add bump version
    
    Co-authored-by: Robert Csakany <robert.csakany@blackbelt.hu>

--- osgi-itest/src/test/java/hu/blackbelt/judo/meta/keycloak/osgi/itest/KarafFeatureProvider.java
+++ osgi-itest/src/test/java/hu/blackbelt/judo/meta/keycloak/osgi/itest/KarafFeatureProvider.java
@@ -1,5 +1,25 @@
 package hu.blackbelt.judo.meta.keycloak.osgi.itest;
 
+/*-
+ * #%L
+ * Judo :: Keycloak :: Model :: OSGi :: Integration Test
+ * %%
+ * Copyright (C) 2018 - 2022 BlackBelt Technology
+ * %%
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ * 
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License, v. 2.0 are satisfied: GNU General Public License, version 2
+ * with the GNU Classpath Exception which is
+ * available at https://www.gnu.org/software/classpath/license.html.
+ * 
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ * #L%
+ */
+
 import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.TimeoutException;
 import org.ops4j.pax.exam.karaf.options.LogLevelOption;
@@ -138,7 +158,8 @@ public class KarafFeatureProvider {
                 vmOption("--add-exports=java.base/sun.net.www.protocol.jar=ALL-UNNAMED"),
                 vmOption("--add-exports=jdk.naming.rmi/com.sun.jndi.url.rmi=ALL-UNNAMED"),
                 vmOption("-classpath"),
-                vmOption("lib/jdk9plus/*" + File.pathSeparator + "lib/boot/*"),
+                vmOption("lib/jdk9plus/*" + File.pathSeparator + "lib/boot/*"
+                        + File.pathSeparator + "lib/endorsed/*"),
                 vmOption("-Xmx2048M"),
                 // avoid integration tests stealing focus on OS X
                 vmOption("-Djava.awt.headless=true"),
